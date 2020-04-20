5f6d8eac31df1dc7ef2dce8f207a9ace2b8f505e
Jasmine Ong <jong@thoughtworks.com>
2020-04-20 11:28:26 +0700

    [Jas] Add jib task for distroless image

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.3.71"
     distribution
     application
+    id("com.google.cloud.tools.jib") version "2.2.0"
 }
 
 application {
@@ -13,10 +14,6 @@ repositories {
     jcenter()
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.sparkjava:spark-kotlin:1.0.0-alpha")
@@ -24,4 +21,24 @@ dependencies {
     testImplementation(kotlin("test-junit"))
     testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.3")
     testImplementation("io.kotest:kotest-assertions-core-jvm:4.0.3")
+}
+
+
+java {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+
+tasks {
+
+    build {
+        dependsOn(jibDockerBuild)
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
 }
